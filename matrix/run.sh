8e4adb4a3b8c8372e0b4726f8fcaa6fecfd19413
takumi uchida <mitauchiku@gmail.com>
2020-07-27 16:07:05 +0900

    new: cleanup

--- matrix/run.sh
+++ matrix/run.sh
@@ -13,54 +13,55 @@ MAP[9]=J
 MAP[10]=K
 
 for i in {0..0}; do
-  for j in {1..1}; do
-        if [ $i != $j ]; then 
-          #before
-          cd ~/Programs/ns-3-dev/matrix
-          python3 ./create_sim.py --OrigNode ${MAP[$i]} --DestNode ${MAP[$j]} >> created
-          python3 ./combine.py
-          cd ~/Programs/ns-3-dev
-          ./waf --run "created --OrigNode=${i} --DestNode=${j}"
-          python3 ./Plot/datarate.py >> ./matrix/odrate.py
-          mkdir ./Data/$i-$j-b
-          cd ~/Programs/ns-3-dev/matrix
-          mv link.* ../Data/$i-$j-b
-          mv route.py ../Data/$i-$j-b
-          rm created
-          rm ../scratch/created.cc
+  for j in {10..10}; do
+        if [ $i != $j ]; then
+           
+          # #before
+          # cd ~/Programs/ns-3-dev/matrix
+          # python3 ./create_sim.py --OrigNode ${MAP[$i]} --DestNode ${MAP[$j]} >> created
+          # python3 ./combine.py
+          # cd ~/Programs/ns-3-dev
+          # ./waf --run "created --OrigNode=${i} --DestNode=${j}"
+          # python3 ./Plot/datarate.py >> ./matrix/odrate.py
+          # mkdir ./Data/$i-$j-b
+          # cd ~/Programs/ns-3-dev/matrix
+          # mv link.* ../Data/$i-$j-b
+          # mv route.py ../Data/$i-$j-b
+          # rm created
+          # rm ../scratch/created.cc
 
-          #latent
-          cd ~/Programs/ns-3-dev/matrix
-          python3 ./create_sim.py --Opt --OdRate latent --OrigNode ${MAP[$i]} --DestNode ${MAP[$j]} >> created
-          python3 ./combine.py
-          cd ~/Programs/ns-3-dev
-          ./waf --run "created --OrigNode=${i} --DestNode=${j}"
-          python3 ./Plot/datarate.py >> ./matrix/odrate_result_latent.py
-          mkdir ./Data/$i-$j-l
-          cd ~/Programs/ns-3-dev/matrix
-          mv link.* ../Data/$i-$j-l
-          mv capas_incd.py ../Data/$i-$j-l
-          mv route.py ../Data/$i-$j-l
-          rm created
-          rm ../scratch/created.cc
+          # #latent
+          # cd ~/Programs/ns-3-dev/matrix
+          # python3 ./create_sim.py --Opt --OdRate latent --OrigNode ${MAP[$i]} --DestNode ${MAP[$j]} >> created
+          # python3 ./combine.py
+          # cd ~/Programs/ns-3-dev
+          # ./waf --run "created --OrigNode=${i} --DestNode=${j}"
+          # python3 ./Plot/datarate.py >> ./matrix/odrate_result_latent.py
+          # mkdir ./Data/$i-$j-l
+          # cd ~/Programs/ns-3-dev/matrix
+          # mv link.* ../Data/$i-$j-l
+          # mv capas_incd.py ../Data/$i-$j-l
+          # mv route.py ../Data/$i-$j-l
+          # rm created
+          # rm ../scratch/created.cc
 
-          #actual
-          cd ~/Programs/ns-3-dev/matrix
-          python3 ./create_sim.py --Opt --OdRate actual --OrigNode ${MAP[$i]} --DestNode ${MAP[$j]} >> created
-          python3 ./combine.py
-          cd ~/Programs/ns-3-dev
-          ./waf --run "created --OrigNode=${i} --DestNode=${j}"
-          python3 ./Plot/datarate.py >> ./matrix/odrate_result_actual.py
-          mkdir ./Data/$i-$j-a
-          cd ~/Programs/ns-3-dev/matrix
-          mv link.* ../Data/$i-$j-a
-          mv capas_incd.py ../Data/$i-$j-a
-          mv route.py ../Data/$i-$j-a
-          rm created
-          rm ../scratch/created.cc
-          mv odrate_result_actual.py ../Data/$i-$j-a
-          mv odrate_result_latent.py ../Data/$i-$j-l
-          mv odrate.py ../Data/$i-$j-b
+          # #actual
+          # cd ~/Programs/ns-3-dev/matrix
+          # python3 ./create_sim.py --Opt --OdRate actual --OrigNode ${MAP[$i]} --DestNode ${MAP[$j]} >> created
+          # python3 ./combine.py
+          # cd ~/Programs/ns-3-dev
+          # ./waf --run "created --OrigNode=${i} --DestNode=${j}"
+          # python3 ./Plot/datarate.py >> ./matrix/odrate_result_actual.py
+          # mkdir ./Data/$i-$j-a
+          # cd ~/Programs/ns-3-dev/matrix
+          # mv link.* ../Data/$i-$j-a
+          # mv capas_incd.py ../Data/$i-$j-a
+          # mv route.py ../Data/$i-$j-a
+          # rm created
+          # rm ../scratch/created.cc
+          # mv odrate_result_actual.py ../Data/$i-$j-a
+          # mv odrate_result_latent.py ../Data/$i-$j-l
+          # mv odrate.py ../Data/$i-$j-b
         fi
     done
 done
