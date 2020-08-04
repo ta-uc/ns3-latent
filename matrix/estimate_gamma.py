2b968f99fa33dd3fde9bc5678a86b0f68efc8e58
takumi uchida <mitauchiku@gmail.com>
2020-08-05 00:06:50 +0900

    mod: gamma estimation Eq

--- matrix/estimate_gamma.py
+++ matrix/estimate_gamma.py
@@ -1,17 +1,23 @@
 import math
-from od_data_before import actual as traffic_before, loss
-from od_data_after import actual as traffic_after
+from od_data_before import actual as traffic_before, loss as loss_before
+from od_data_after import actual as traffic_after, loss as loss_after
 
 def estimate_gamma():
   gamma = []
   for od in traffic_before.keys():
-    od_traffic_bf_te = traffic_before[od] # 経路制御前観測トラヒック
-    od_traffic_at_te = traffic_after[od] # 経路制御後観測トラヒック
-    od_packet_loss = loss[od] # 経路制御前観測パケットロス率
-    if od_packet_loss != 0 and od_traffic_bf_te != od_traffic_at_te:
-      gamma.append(math.log(od_traffic_bf_te / od_traffic_at_te) / od_packet_loss)
+    od_traffic_before = traffic_before[od]  # 経路制御前観測トラヒック
+    od_traffic_after = traffic_after[od]    # 経路制御後観測トラヒック
+    od_packet_loss_before = loss_before[od] # 経路制御前観測パケットロス率
+    od_packet_loss_after = loss_before[od]  # 経路制御前観測パケットロス率
+
+    # if od_packet_loss_before != 0 and od_traffic_before != od_traffic_after:
+    if od_packet_loss_before - od_packet_loss_after != 0:
+      gamma.append(math.log(od_traffic_before / od_traffic_after) / (od_packet_loss_before - od_packet_loss_after))
+  
   gamma_mean = sum(gamma) / len(gamma)
+
   with open("estimated_gamma.txt","w") as egf:
     print(repr(gamma), file=egf)
     print(gamma_mean, file=egf)
+
   return gamma_mean
