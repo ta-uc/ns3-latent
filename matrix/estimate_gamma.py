b7be45e20b97eb73f493bb083a73b2c840893a44
takumi uchida <mitauchiku@gmail.com>
2020-08-06 01:02:46 +0900

    fix: ログエラー

--- matrix/estimate_gamma.py
+++ matrix/estimate_gamma.py
@@ -8,17 +8,18 @@ def estimate_gamma():
     od_traffic_before = traffic_before[od]  # 経路制御前観測トラヒック
     od_traffic_after = traffic_after[od]    # 経路制御後観測トラヒック
     od_packet_loss_before = loss_before[od] # 経路制御前観測パケットロス率
-    od_packet_loss_after = loss_after[od]  # 経路制御前観測パケットロス率
-
-    if (od_packet_loss_before - od_packet_loss_after) != 0:
-      gamma.append(math.log(od_traffic_before / od_traffic_after) / (od_packet_loss_before - od_packet_loss_after))
+    od_packet_loss_after = loss_after[od]   # 経路制御前観測パケットロス率
+    if (od_packet_loss_before - od_packet_loss_after) != 0 and od_traffic_before / od_traffic_after > 0:
+      gamma.append(math.log((od_traffic_before / od_traffic_after)) / (od_packet_loss_before - od_packet_loss_after))
   
   if len(gamma) != 0:
     gamma_mean = sum(gamma) / len(gamma)
-  else: 
+  else:
     gamma_mean = 0
   with open("estimated_gamma.txt","w") as egf:
     print(repr(gamma), file=egf)
     print(gamma_mean, file=egf)
 
   return gamma_mean
+
+estimate_gamma()
